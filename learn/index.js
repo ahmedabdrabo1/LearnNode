d192f8e08e4ca378971f8472b3ec2b480182eecc
Ahmed Abdrabo <ahmed.abdrabo935@gmail.com>
2026-06-23 22:31:43 +0300

    HandlePost

--- learn/index.js
+++ learn/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const myData = require("./models/myDb.js");
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/myDatabase");
+    await mongoose.connect("mongodb://localhost:27017");
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed", error);
@@ -19,7 +21,19 @@ const connectDB = async () => {
 app.get("/", (req, res) => {
   res.sendFile("./view/home.html", { root: __dirname });
 });
+app.post('/', async (req, res) => {
+  console.log("POST body:", req.body);
+  const username = new myData({ userName: req.body.userName });
 
+  try {
+    const savedUser = await username.save();
+    console.log("Saved user:", savedUser);
+    res.redirect("/");
+  } catch (error) {
+    console.error("Failed to save user:", error);
+    res.status(500).send("Server error");
+  }
+});
 connectDB().then(() => {
   app.listen(port, () => {
     console.log(`Server running: http://localhost:${port}`);
---
+++ learn/models/myDb.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  userName: { type: String, required: true },
+});
+const myData = mongoose.model("myData", UserSchema);
+module.exports = myData;
